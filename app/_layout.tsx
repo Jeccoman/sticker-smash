5d08e8c38f708b4729af93faaf7086bbb015ea33
Jacob Julius <jeccopatrick@outlook.com>
2025-01-15 10:40:53 +0300

    :app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,8 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack>
+    <Stack.Screen name="Index" options={{title: 'Home'}} />
+    <Stack.Screen name="About" options={{title: 'About'}} />
+  </Stack>
 }
